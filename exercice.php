fac5eb4ddb9d1878a1c65b0da3c8e1c246d19f8d
aurianebgnl <auriane.bgnl@gmail.com>
2025-05-22 11:33:08 +0200

    etapes 3 et 4 - SESSION

--- exercice.php
+++ exercice.php
@@ -1,3 +1,5 @@
+<?php session_start(); ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -9,23 +11,33 @@
     
     <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
 
-    <h1>Bonjour <?php 
-
-    if(isset($_GET['first_name'])){
-        echo $_GET['first_name'];
-    } else if(isset($_POST['first_name'])){
-        echo $_POST['first_name'] ;
-    } else {
-        echo "anonyme";
-    }
-    ?></h1>
-
     <form action="exercice.php" method="post">
         <p>Votre nom : <input type="text" name="first_name" /></p>
         <p><input type="submit" value="OK"></p>
     </form>
 
+    <h1>Bonjour <?php 
 
+    if(isset($_GET['first_name'])){
+        $_SESSION['first_name'] = $_GET['first_name'];
+        echo $_SESSION['first_name'] ;
+    } else if(isset($_POST['first_name'])){
+        $_SESSION['first_name'] = $_POST['first_name'];
+        echo $_SESSION['first_name'] ;
+    } else if(isset($_SESSION['first_name'])){
+        echo $_SESSION['first_name'] ;
+    }  else {
+        echo "anonyme";
+    }
+    ?></h1>
 
+    <!-- <?php
+    print_r($_SESSION);
+    ?> -->
+
+    <form action="clear-session.php" method="POST">
+        <input type="submit" value="Reset" />
+    </form>
+   
 </body>
 </html>
